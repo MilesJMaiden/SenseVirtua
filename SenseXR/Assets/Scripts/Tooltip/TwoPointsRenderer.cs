55628c4cb404426836511bee543fc69440542d24
Runtomaitorun <yeahmy1022@gmail.com>
2024-06-24 14:30:18 +0100

    tooltip rotation tweaks!!

--- SenseXR/Assets/Scripts/Tooltip/TwoPointsRenderer.cs
+++ SenseXR/Assets/Scripts/Tooltip/TwoPointsRenderer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class TwoPointsRenderer : MonoBehaviour
 {
     public Transform pointA;
