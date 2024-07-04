af7ea78294cde3683a5c65204ef8173ada829b91
Runtomaitorun <yeahmy1022@gmail.com>
2024-07-05 01:32:41 +0800

    all stupid prefabs piece up glaze together

---
+++ SenseXR/Assets/Scripts/Interactables/GlazeRepair.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GlazeRepair : MonoBehaviour
+{
+    public GameObject otherPiece;
+    public GameObject leftPiece;
+    public GameObject wholePiece;
+    public bool isLeftHandPiece;
+    public MeshRenderer wholeLeftMeshrenderer;
+
+    private XRGrabInteractable grabInteractable;
+    public bool isCombined = false;
+
+    void Start()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.onSelectExited.AddListener(OnRelease);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // when trigger entered check if that is the other piece
+
+        Debug.Log("entering");
+        if (collision.gameObject == otherPiece.gameObject && !isCombined)
+        {
+            isCombined = true;
+        }
+        else
+            Debug.Log("not combined!!");
+    }
+    private void Update()
+    {
+        if(isCombined)
+        {
+            CombinePieces();
+        }
+    }
+
+    public void OnRelease(XRBaseInteractor interactor)
+    {
+        // if it is, combine the two when release any of the 2
+        if (isCombined)
+        {
+            CombinePieces();
+        }
+
+    }
+
+    public void CombinePieces()
+    {
+              
+        // Disable the left
+        leftPiece.SetActive(false);
+
+        // Enable the right-left mesh
+        wholeLeftMeshrenderer.enabled = true;
+
+        
+    }
+
+
+   
+}
