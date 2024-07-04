c69596983b6afbe8e477e2697acf99afe616288f
HjoonJ <150797637+HjoonJ@users.noreply.github.com>
2024-07-04 05:30:11 +0100

    Start Scene

---
+++ SenseXR/Assets/Scripts/StartScene/SceneTransitionManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public FadeScreen fadeScreen;
+    public static SceneTransitionManager singleton;
+
+    private void Awake()
+    {
+        if (singleton && singleton != this)
+            Destroy(singleton);
+
+        singleton = this;
+    }
+
+    public void GoToScene(string sceneName)
+    {
+        StartCoroutine(GoToSceneRoutine(sceneName));
+    }
+
+    IEnumerator GoToSceneRoutine(string sceneName)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+
+        //Launch the new scene
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void GoToSceneAsync(string sceneName)
+    {
+        StartCoroutine(GoToSceneAsyncRoutine(sceneName));
+    }
+
+    IEnumerator GoToSceneAsyncRoutine(string sceneName)
+    {
+        fadeScreen.FadeOut();
+        //Launch the new scene
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        float timer = 0;
+        while(timer <= fadeScreen.fadeDuration && !operation.isDone)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        operation.allowSceneActivation = true;
+    }
+}
