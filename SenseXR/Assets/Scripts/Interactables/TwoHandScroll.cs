9196efd63fa49bea595800b654bd44c437cb987d
Runtomaitorun <yeahmy1022@gmail.com>
2024-07-25 20:45:26 +0100

    shader-slider snyc

--- SenseXR/Assets/Scripts/Interactables/TwoHandScroll.cs
+++ SenseXR/Assets/Scripts/Interactables/TwoHandScroll.cs
@@ -11,35 +11,83 @@ public class TwoHandScroll : MonoBehaviour
 
     public GameObject cover;
     public GameObject slideScroll;
-    private XRSlider xrSlider;
+    
+    [SerializeField]
+    XRSlider xrSlider;
 
+    [SerializeField]
+    Material coverMat;
 
-    private Material coverMat;
-
-    private void OnEnable()
+    protected void OnEnable()
     {
-        Material coverMat = cover.GetComponent<Material>();
-        xrSlider = slideScroll.GetComponent<XRSlider>();
-        Debug.Log("the scroll is awaken");
+        if (cover != null)
+        {
+            coverMat = cover.GetComponent<MeshRenderer>().material;
+            Debug.Log("Cover material assigned.");
+        }
+        else
+        {
+            Debug.LogError("Cover GameObject is not assigned.");
+        }
+
+        // Check if slideScroll is assigned and get XRSlider component
+        if (slideScroll != null)
+        {
+            xrSlider = slideScroll.GetComponent<XRSlider>();
+            if (xrSlider != null)
+            {
+                Debug.Log("XRSlider component assigned.");
+            }
+            else
+            {
+                Debug.LogError("XRSlider component not found on slideScroll.");
+            }
+        }
+        else
+        {
+            Debug.LogError("SlideScroll GameObject is not assigned.");
+        }
+
+        Debug.Log("The scroll is awaken");
+        
         ConnectControlEvents();
         InitialControls();
     }
 
-    private void OnDisable()
+    protected void OnDisable()
     {
         DisconnectControlEvent();
     }
 
     void ConnectControlEvents()
     {
-        xrSlider.onValueChange.AddListener(UpdateUnfoldingValue);
-        Debug.Log("Slider is changing Value");
+        if (xrSlider != null)
+        {
+            xrSlider.onValueChange.AddListener(UpdateUnfoldingValue);
+            Debug.Log("Listener added to XRSlider.");
+        }
+        else
+        {
+            Debug.LogError("XRSlider is null. Listener not added.");
+        }
     }
+    private void Update()
+    {
 
+            UpdateUnfoldingValue(xrSlider.value);
+
+    }
     void DisconnectControlEvent()
     {
-        xrSlider.onValueChange.RemoveListener(UpdateUnfoldingValue);
-        Debug.Log("Listener is removed from slider");
+        if (xrSlider != null)
+        {
+            xrSlider.onValueChange.RemoveListener(UpdateUnfoldingValue);
+            Debug.Log("Listener removed from XRSlider.");
+        }
+        else
+        {
+            Debug.LogError("XRSlider is null. Listener not removed.");
+        }
     }
 
     void InitialControls()
@@ -50,6 +98,7 @@ public class TwoHandScroll : MonoBehaviour
 
     public void UpdateUnfoldingValue(float value)
     {
+        Debug.Log("the XRSlider Value is" + xrSlider.value);
         Debug.Log("Updating unfolding value!!!");
         value = xrSlider.value;
         unfoldingValue = max_UnfoldingValue - value * (max_UnfoldingValue - min_UnfoldingValue);
